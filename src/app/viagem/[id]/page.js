701ab9cf8331cd2db595e5015bcef33b1e46eb52
JoaoPe22 <joao.canezin22@gmail.com>
2024-07-17 17:27:58 -0400

    Estilização final

--- src/app/viagem/[id]/page.js
+++ src/app/viagem/[id]/page.js
@@ -1,22 +1,28 @@
 import Image from "next/image";
-
+import styles from "./viagem.module.css";
 export default async function Viagem({ params }) {
   const response = await fetch("http://localhost:3000/api/" + params.id);
   const data = await response.json();
   return (
-    <div>
-      {
-        <Image
-          src={data.imagem}
-          width={350}
-          height={200}
-          alt="imagem de produto"
-        ></Image>
-      }
-      <h3>{data.lugar}</h3>
-      <p>{data.pais}</p>
-      <h2>R${data.preco}</h2>
-      <p>{data.tipo}</p>
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        {
+          <Image
+            src={data.imagem}
+            width={350}
+            height={200}
+            alt="imagem de produto"
+            className={styles.Image}
+          ></Image>
+        }
+      </div>
+
+      <div className={styles.textContainer}>
+        <h3 className={styles.title}>{data.lugar}</h3>
+        <p className={styles.country}>{data.pais}</p>
+        <h2 className={styles.price}>R${data.preco}</h2>
+        <p className={styles.type}>{data.tipo}</p>
+      </div>
     </div>
   );
 }
